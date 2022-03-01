e2681b84437c908119a8627aa9f77f79c3acc7d0
Joseph <joeyparlamenti@yahoo.com>
2022-02-28 19:43:25 -0500

    commit tree 6318710639b6b78d1510f7b1c6c82880283a5722

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // code your solution here
+
+const timeline = [
+    { year: "2002", result: "T"},
+    { year: "2003", result: "L"},
+    { year: "2004", result: "c"},
+    { year: "2005", result: "L"},
+    { year: "2006", result: "W"}
+]
+
+
+// function superbowlWin(date) {
+//     if (date.result === "W") {
+//         return date.year;
+//     }
+// }
+
+
+function superbowlWin(arr){
+    return (arr.find(arr => arr.result === "W")).year;
+}
+
+
+
+console.log(superbowlWin(timeline))
+
+//onsole.log(timeline.find(superbowlWin));
